b0271ec67532b41f75dc1b0d437522c6d3f1daf0
lucazacca <zaccarialuca86@gmail.com>
2023-05-16 15:39:27 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='grid_map_python',
+version='0.1',
+description='A python package for a simple grid map',
+url='#',
+author='max',
+install_requires=[],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
